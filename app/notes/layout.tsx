d8bf14d2d1123cbfb72006f0921312a90f5bc862
Andrii888 <aotk888@gmail.com>
2026-02-24 23:21:35 +0200

    fix-notes-layout

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -1,16 +1,6 @@
 import type { ReactNode } from "react";
+import css from "./LayoutNotes.module.css";
 
-export default function NotesLayout({
-  children,
-  modal,
-}: {
-  children: ReactNode;
-  modal: ReactNode;
-}) {
-  return (
-    <>
-      {children}
-      {modal}
-    </>
-  );
+export default function NotesLayout({ children }: { children: ReactNode }) {
+  return <div className={css.container}>{children}</div>;
 }
